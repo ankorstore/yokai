db4828d82035712a5959ac79f9aa2460b01d441c
Jonathan Vuillemin <ekkinox@gmail.com>
2024-03-14 11:35:20 +0100

    feat(httpserver): Updated dependencies

--- httpserver/trace_test.go
+++ httpserver/trace_test.go
@@ -6,21 +6,8 @@ import (
 
 	"github.com/ankorstore/yokai/httpserver"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/trace"
 	"go.opentelemetry.io/otel/trace/noop"
 )
-
-type tracerProviderMock struct {
-	mock.Mock
-}
-
-func (m *tracerProviderMock) Tracer(string, ...trace.TracerOption) trace.Tracer {
-	args := m.Called()
-
-	return otel.GetTracerProvider().Tracer(args.String(0))
-}
 
 func TestAnnotateTracerProviderWithNonSdkTracerProvider(t *testing.T) {
 	t.Parallel()
