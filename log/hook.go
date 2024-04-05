9b5bed2886a5ea3126fda316e3c1d898ee5a1011
Jonathan Vuillemin <ekkinox@gmail.com>
2024-04-05 14:42:00 +0200

    feat(fxlog): Added log caller info

--- log/hook.go
+++ log/hook.go
@@ -1,16 +1,11 @@
 package log
 
 import (
-	"fmt"
 	"github.com/rs/zerolog"
-	"runtime"
 )
 
 type CallerInfoHook struct{}
 
 func (h CallerInfoHook) Run(e *zerolog.Event, l zerolog.Level, msg string) {
-	_, file, line, ok := runtime.Caller(1)
-	if ok {
-		e.Str("caller", fmt.Sprintf("%s:%d", file, line))
-	}
+	e.Caller(0)
 }
