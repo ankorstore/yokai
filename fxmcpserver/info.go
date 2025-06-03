3b0ff215dd4668046a5dfd0017b27467e6ac01e5
Jonathan Vuillemin <ekkinox@gmail.com>
2025-06-03 10:21:37 +0200

    feat(fxmcpserver): Provided streamable HTTP transport

--- fxmcpserver/info.go
+++ fxmcpserver/info.go
@@ -5,46 +5,52 @@ import (
 	"github.com/ankorstore/yokai/fxmcpserver/server"
 	"github.com/ankorstore/yokai/fxmcpserver/server/sse"
 	"github.com/ankorstore/yokai/fxmcpserver/server/stdio"
+	"github.com/ankorstore/yokai/fxmcpserver/server/stream"
 )
 
 // MCPServerModuleInfo is the MCP server module info.
 type MCPServerModuleInfo struct {
-	config      *config.Config
-	registry    *server.MCPServerRegistry
-	sseServer   *sse.MCPSSEServer
-	stdioServer *stdio.MCPStdioServer
+	config              *config.Config
+	registry            *server.MCPServerRegistry
+	steamableHTTPServer *stream.MCPStreamableHTTPServer
+	sseServer           *sse.MCPSSEServer
+	stdioServer         *stdio.MCPStdioServer
 }
 
 // NewMCPServerModuleInfo returns a new MCPServerModuleInfo instance.
 func NewMCPServerModuleInfo(
 	config *config.Config,
 	registry *server.MCPServerRegistry,
+	steamableHTTPServer *stream.MCPStreamableHTTPServer,
 	sseServer *sse.MCPSSEServer,
 	stdioServer *stdio.MCPStdioServer,
 ) *MCPServerModuleInfo {
 	return &MCPServerModuleInfo{
-		config:      config,
-		registry:    registry,
-		sseServer:   sseServer,
-		stdioServer: stdioServer,
+		config:              config,
+		registry:            registry,
+		steamableHTTPServer: steamableHTTPServer,
+		sseServer:           sseServer,
+		stdioServer:         stdioServer,
 	}
 }
 
-// Name return the name of the module info.
+// Name returns the name of the module info.
 func (i *MCPServerModuleInfo) Name() string {
 	return ModuleName
 }
 
 // Data return the data of the module info.
 func (i *MCPServerModuleInfo) Data() map[string]any {
+	streamableHTTPServerInfo := i.steamableHTTPServer.Info()
 	sseServerInfo := i.sseServer.Info()
 	stdioServerInfo := i.stdioServer.Info()
 	mcpRegistryInfo := i.registry.Info()
 
 	return map[string]any{
 		"transports": map[string]any{
-			"sse":   sseServerInfo,
-			"stdio": stdioServerInfo,
+			"stream": streamableHTTPServerInfo,
+			"sse":    sseServerInfo,
+			"stdio":  stdioServerInfo,
 		},
 		"capabilities": map[string]any{
 			"tools":     mcpRegistryInfo.Capabilities.Tools,
